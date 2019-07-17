e32446c2125555fb6c42f9d383d2d2cc18972e9d
LEE KYUNGIL <dlruddlf07@gmail.com>
2019-07-17 17:41:41 +0900

    Create signup & article area layout
    * Use masonry layout for article area
    * Create Problem.vue component
    * Create Modal_Signup.vue component
    * Create css directory in assets

--- src/components/ContextMenu.vue
+++ src/components/ContextMenu.vue
@@ -1,26 +1,34 @@
 <template>
     <nav class="left">
+        <ModalLogin></ModalLogin>
+        <ModalSignup></ModalSignup>
         <button class="close_context"><i class="material-icons">chevron_left</i></button>
         <section>
             <div id="profile_img"></div>
-            <a href="#" class="user_login">로그인</a>
-            <a href="#" class="user_signup">회원가입</a>
+            <a href="#" class="context__btn user_login">로그인</a>
+            <a href="#" class="context__btn user_signup">회원가입</a>
             <footer>
                 <a class="link" href="https://github.com/interviewassist" target="blank_">
                     <i class="github-icon"></i> github
                 </a>
             </footer>
         </section>
-        
     </nav>    
 </template>
 <script>
+import ModalLogin from "./Modal"
+import ModalSignup from "./Modal__Signup"
 export default {
     name : "ContextMenu"
     ,data: function(){
         return{
             // showContextMenu : true
+            modal_type:''
         }
+    }
+    ,components:{
+        ModalLogin
+        ,ModalSignup
     }
     ,mounted : function(){
         var close_context = document.querySelector("nav>.close_context");
@@ -41,94 +49,33 @@ export default {
         });
 
         var user_login = document.getElementsByClassName("user_login")[0];
+        var user_signup = document.getElementsByClassName("user_signup")[0];
+        
+        var modal = document.getElementsByClassName("modal");
+        var modal_login = modal[0];
+        var modal_signup = modal[1];
 
         user_login.addEventListener("click",()=>{
-            var modal = document.getElementsByClassName("modal")[0];
+            modal_login.classList.toggle("show_modal");
+            if(modal_signup.classList[1]==="show_modal"){
+                modal_signup.classList.toggle("show_modal");
+            }
+        });
 
-            modal.classList.toggle("show_modal");
+        user_signup.addEventListener("click",()=>{
+            modal_signup.classList.toggle("show_modal");
+            if(modal_login.classList[1]==="show_modal"){
+                modal_login.classList.toggle("show_modal");
+            }
         });
     }
 }
 </script>
 <style>
-    section>nav.left{
-        position:relative;
-        width: 0;
-        height:inherit;
-        transition : all 0.4s ease-out;
-        border-right: 1px solid rgba(200,200,200,0.5);
-    }
-    nav.left>.close_context{  
-        background : transparent;
-        height: 30px;
-        position:absolute;
-        right:0;
-        top:0;
-        width: 30px;
-
-    }
-    .close_context>i{
-        font-size:24px;
-        transition: all 0.4s ease;
-    }
-    .close_context>i:hover{
-        font-weight: bold;
-    }
-    #profile_img{
-        width:50px;
-        height:50px;
-        background: #333;
-        border-radius: 50%;
-        margin : 0 auto 0.6rem auto;
-        margin-left:auto;
-        margin-right:auto;
-
-        cursor:pointer;
-    }
-    nav.left>section{
-        margin: 4rem 0 0 0;
-        text-align: center;
-        height:inherit;
-        width:inherit;
-        opacity: 0;
-    }
-    nav>section *{
-        opacity: inherit;
-        transition: all 0.3s;
-    }
-    .user{
-        color: rgba(4,5,9,0.8);
-        text-decoration: none;
-        font-size:0.8rem;
-    }
-    .user:hover{
-        text-decoration: underline;
-    }
-
-    
-    section>footer{
-        position:absolute;
-        width:inherit;
-        bottom:0;
-        margin : 0 auto 1rem auto;
-    }
-    .link{
-        text-decoration: none;
-        color:rgba(4,5,9,0.8);
-    }
-    .link:hover{
-        text-decoration: underline;
-
-    }
-    .github-icon{
-        display:inline-block;
-        background-image : url("../assets/icons/github-square-brands.svg");
-        background-repeat: no-repeat;
-        height:16px;
-        width:16px;
-        position:relative;
-        bottom:-4px;
-    }
+    @import url('../assets/css/modal.css');
+</style>
+<style scoped>
+    @import url('../assets/css/context.css');
 </style>
 
 
--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -5,12 +5,12 @@
             <header>
                 <img class="modal_logo" src="../assets/logo2.png">
             </header>
-            <form id="login_form">
+            <form id="login_form" method="get" action="#">
                 <input class="modal_input" name="user_id"
-                        type="text" placeholder="아이디" autofocus>
+                        type="email" placeholder="이메일" autofocus required>
                 <input class="modal_input" name="user_pw"
-                        type="password" placeholder="비밀번호">
-                <button class="login_btn">로그인</button>
+                        type="password" placeholder="비밀번호" required>
+                <input type="submit" class="login_btn" value="로그인">
                 <a class="forgot"
                     href="#">아이디 및 비밀번호 찾기</a>
             </form>
@@ -36,18 +36,18 @@ export default {
     }
     ,mounted:function(){
         var close_modal = document.getElementsByClassName("close_modal")[0];
-        var modal = document.getElementsByClassName("modal")[0];
+        var modal = document.getElementsByClassName("modal");
+        var modal_login = modal[0];
+        var modal_signup = modal[1];
 
         close_modal.addEventListener("click",(evt)=>{
-            modal.classList.toggle("show_modal");
+            modal_login.classList.toggle("show_modal");
         });
 
 
-        modal.addEventListener("click",function(evt){
+        modal_login.addEventListener("click",function(evt){
             if(evt.target===evt.currentTarget){
                 evt.currentTarget.classList.toggle("show_modal");
-                console.log("clicked modal................");
-                console.log(evt.currentTarget.tagName);
             }
 
         });
@@ -62,162 +62,8 @@ export default {
     }
 }
 </script>
-
 <style>
-@media (min-width: 0px) and (max-width: 414px){
-    .modal>.modal_wrap{
-        width:90vw;
-        height:90vh;
-    }
-    .modal_wrap>header{
-        margin:10vh auto 0 auto;
-    }
-    .modal_wrap>#login_form {
-        flex:1;
-        margin-bottom:20vh;
-    }
-    header>.modal_logo{
-        width:90%;
-    }
-    .social_btn_group{
-        flex:0;
-        
-    }
-}
-@media (min-width: 415px){
-    .modal>.modal_wrap{
-        width:50vh;
-        height:80vh;
-    }
-    .modal_wrap>header{
-        margin:9vh auto 0 auto;
-    }
-    .modal_wrap>#login_form {
-        flex:2;
-        margin-bottom:4vh;
-    }
-    header>.modal_logo{
-        width:50%;
-    }
-    .social_btn_group{
-        flex:1;
-        
-    }
-}
-    div.modal{
-        display:none;
-        position:fixed;
-        width:100vw;
-        height:100vh;
-        background:rgba(4,5,9,0.8);
-        z-index: 1;
-    }
-    div.show_modal{
-        display:flex;
-        flex-direction: column;
-        justify-content: center;
-    }
-    .modal>.modal_wrap{
-        background:#FBFAF6;
-        border-radius: 2vh;
-        display:flex;
-        flex-direction: column;
-        position:relative;
-        margin:auto;
-        padding: 0 2vh ;
-    }
-    .modal_wrap>.close_modal{
-        display:inline-block;
-        position: absolute;
-        top:0;
-        right:0;
-        color:rgba(4,5,9,0.8);
-        margin:2vh;
-    }
-    .close_modal>.material-icons{
-        font-size: 4vh;
-    }
-    .modal_wrap>header{
-        display:flex;
-        position:absolute;
-        top: 0;
-        left:0;
-        width: 100%;
-        font-size:8vmin;
-        font-weight: bold;
-        text-align:center;
-        background:rgba(4,5,9,0.8);
-
-    }
-    header>.modal_logo{
-        margin: 0 auto 0 auto;
-    }
-    .modal_wrap>#login_form{
-        border-radius: inherit;
-        display:flex;
-        flex-direction: column;
-        width:100%;
-        height: 100%;
-        justify-content:flex-end;
-    }
-    #login_form>.modal_input{
-        border-radius: 5rem;
-        padding: 0 1rem 0 1rem;
-        display:block;
-        height:5vh;
-        background:#fff;
-        margin: 0 0 1vh 0;
-        box-shadow: 0px 1px 1px 0px gray;
-
-    }
-    #login_form>.modal_input:hover{
-        box-shadow: 0px 2px 2px 1px rgba(4,5,9,0.3);
-    }
-
-    #login_form>.login_btn{
-        width: 20vh;
-        height:4vh;
-        margin:4vh auto 0 auto;
-        background: rgba(255, 166, 0, 0.7);
-        border-radius: 5rem;
-        font-weight: bold;
-    }
-    #login_form>.login_btn:hover{
-        background: rgba(255, 166, 0, 0.75);
-        box-shadow: 0px 2px 2px 1px rgba(4,5,9,0.3);
-    }
-    #login_form>.forgot{
-        font-size: 1.1vh;
-        color:inherit;
-        text-decoration: none;
-        margin:1rem auto 0 auto;
-    }
-    #login_form>.forgot:hover{
-        text-decoration: underline;
-    }
-    /* Social Login Buttons*/
-    .social_btn_group{
-        display:flex;
-        flex-direction: column;
-        justify-content: flex-end;
-    }
-    .social_btn_group>*{
-        border-radius: 5rem;
-        padding: 1vh 0 1vh 0;
-        margin-bottom:2vh;
-    }
-    .github_login{
-        background:#24292e;
-    }
-
-    .google_login{
-        background:white;
-        box-shadow: 1px 1px 1px rgba(0,0,0,.3);
-    }
-
-    .github_login>*
-    ,.google_login>*{
-        height:3vh;
-    }
+@import url('../assets/css/modal__login.css');
 </style>
+
 
