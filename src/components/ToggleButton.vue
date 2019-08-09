1a976691931fd33b7e2d8e4c50e9dfa64d2c8ba2
LEE KYUNGIL <dlruddlf07@gmail.com>
2019-08-09 12:21:02 +0900

    Create toggleButton for mainPage's list

---
+++ src/components/ToggleButton.vue
@@ -0,0 +1,26 @@
+<template>
+  <section class="listToggle">
+    <div class="listToggle__innerwrap">
+      <button class="listToggle__button listToggle__single listToggle__single--selected z20">
+        <i class="material-icons">view_day</i>
+      </button>
+      <button class="listToggle__button listToggle__double z20">
+        <i class="material-icons">dashboard</i>
+      </button>
+      <div class="listToggle__now"></div>
+    </div>
+  </section>
+</template>
+<script>
+import { Mounted } from "../assets/js/toggleButton";
+export default {
+  name: "ToggleButton",
+  data: () => ({}),
+  mounted: Mounted
+};
+</script>
+<style>
+@import url("../assets/css/togglemenu.css");
+</style>
+
+
